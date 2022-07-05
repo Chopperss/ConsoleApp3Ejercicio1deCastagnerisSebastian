6bbc1d346f3a18acb7c2fb7096398a3725ceba9b
sac14 <sac14@LAPTOP-U1GTBSUE>
2022-07-05 18:52:35 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp3Ejercicio1deCastagnerisSebastian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double velocidad, tiempo, metro, distancia, metros;
+
+            Console.WriteLine("Ingrese la velocidad del Automovil");
+            velocidad = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese tiempo de viaje:");
+            tiempo = double.Parse(Console.ReadLine());
+
+            distancia = velocidad * tiempo;
+            metros = distancia * 0.001;
+
+
+            Console.WriteLine($"El Automovil recorrió {distancia} metros.");
+            Console.WriteLine($"Expresado en kilometros, el Automovil recorrió {metros} kms.");
+
+            Console.ReadLine();
+        }
+        
+    }
+}
